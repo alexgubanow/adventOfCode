ac03900efb5e9c482dcd709f61fe8e38145da939
Oleksandr Hubanov <o.hubanov@precizika.com>
2017-12-04 10:09:11 +0200

    init

---
+++ day4p2/day4p2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace day4p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please give input file!");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Please give input file!");
+                return;
+            }
+            using (var fileStream = File.OpenRead(args[0]))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, 128))
+            {
+                String line;
+                int numTrue = 0;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] arr = line.Split(' ');
+                    bool n = false;
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        for (int k = i + 1; k < arr.Length; k++)
+                        {
+                            if (arr[i].Length == arr[k].Length)
+                            {
+                                if (arr[i] == arr[k] || getCharSum(arr[i]) == getCharSum(arr[k]))
+                                {
+                                    n = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (!n)
+                    {
+                        numTrue++;
+                    }
+                }
+                Console.WriteLine(numTrue + " passphrases are valid");
+            }
+        }
+
+        static int getCharSum(string str1)
+        {
+            int sum = 0;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                sum += (int)str1[i];
+            }
+            return sum;
+        }
+    }
+}
